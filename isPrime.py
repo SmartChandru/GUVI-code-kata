2cc75f905fd864786d4a5cc65efb76e362c0e924
SmartChandru <51984083+SmartChandru@users.noreply.github.com>
2019-09-11 16:16:59 +0530

    Create isPrime.py

---
+++ isPrime.py
@@ -0,0 +1,8 @@
+def isPrime(n): 
+  if n <= 1 :  return False
+  if n <= 3 :  return True 
+  if n%2 == 0 or n%3 == 0 : return False 
+  for i in range(5,n//2,6):
+    if n%i == 0 or n%(i+2) == 0 : 
+      return False 
+  return True 
