6fc3c941f1d36ed6680ddf999b8c7dc869fbd515
SmartChandru <51984083+SmartChandru@users.noreply.github.com>
2019-11-26 17:58:08 +0530

    No of subarray whose max element is greater than k
    You are provided with an array. Your task is to print the count of subarray whose maximum element is strictly greater than ‘k’.
    
    Input Description:
    First line contains two element ‘N’ and ‘k’.Second line contains ‘n’ space separated numbers.
    
    Output Description:
    Print the count of subarray whose maximum element’s value is strictly greater than ‘k’.
    
    Sample Input :
    5 4
    1 6 7 8 9
    
    Sample Output :
    14

---
+++ NsubarrayMaxElement.py
@@ -0,0 +1,20 @@
+def count(a, n, k): 
+  s = 0
+  i = 0
+  while (i < n): 
+    if (a[i] > k): 
+      i = i + 1
+      continue
+    c = 0
+    while (i < n and a[i] <= k):     
+      i = i + 1
+      c = c + 1
+      #print(i,c,s)
+    s = s + ((c*(c + 1))//2)
+    #print(i,c,s,"*")
+  return (n*(n + 1)//2 - s)
+  return s
+n,k = map(int,input().split())
+a = list(map(int,input().split()))
+print(count(a, n, k)) 
+  
